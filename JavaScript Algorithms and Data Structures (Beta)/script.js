22ad66d1a0cddb9906918ac8921e76708cefe45c
siserin <siserin.desarrollo@gmail.com>
2024-09-26 12:05:35 -0600

    Step 27

--- JavaScript Algorithms and Data Structures (Beta)/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/script.js
@@ -8,10 +8,27 @@ console.log("#########");
 let developer = "Naomi";
 console.log(developer);
 
-let cities = ["London", "New York", "Munbai"];
+/*Step 27
+
+/*let cities = ["London", "New York", "Munbai"];
 console.log(cities);
 cities[2] = "Mexico City";
 console.log(cities);
 let rows = ["Naomi", "Quincy", "CamperChan"];
 rows.push("freeCodeCamp");
+console.log(rows);*/
+
+/*
+Step 27
+Another method essential for this project is the .pop() method. It removes the last element from an array and returns that element.
+
+When a method returns a value, you can think of it as giving the value back to you, making it available for use in other parts of your code.
+
+Create a new variable called popped and assign it the result of rows.pop(). Then, log popped to the console.*/
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let popped;
+popped = rows.pop();
+console.log(popped);
 console.log(rows);
