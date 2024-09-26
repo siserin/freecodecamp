1380d893d0c0ab8108c91a4361c0e8ad83b0f36d
siserin <siserin.desarrollo@gmail.com>
2024-09-26 13:23:53 -0600

    Step 35

--- JavaScript Algorithms and Data Structures (Beta)/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/script.js
@@ -138,12 +138,31 @@ Example Code
 for (let index = 100; "second"; "third") {
 
 }
-Replace the string "iterator" with a let declaration for the variable i. Assign it the value 0 to start. This will give the i variable the value 0 the first time your loop runs.*/
+Replace the string "iterator" with a let declaration for the variable i. Assign it the value 0 to start. This will give the i variable the value 0 the first time your loop runs.
 
 const character = "#";
 const count = 8;
 const rows = [];
 
 for (let i = 0; "condition"; "iteration") {
+
+}
+
+Step 35
+
+The condition of a for loop tells the loop how many times it should iterate. When the condition becomes false, the loop will stop.
+
+In JavaScript, a Boolean value can be either true or false. These are not strings - you will learn more about the difference later on.
+
+For now, you will use the less than operator (<). This allows you to check if the value on the left is less than the value on the right. For example, count < 3 would evaluate to true if count is 2, and false if count is 4.
+
+Replace your "condition" string with a condition to check if i is less than count.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; "iteration") {
 
 }
