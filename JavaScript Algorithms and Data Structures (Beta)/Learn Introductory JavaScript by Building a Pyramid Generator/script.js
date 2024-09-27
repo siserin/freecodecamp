e8d93b2707d11448d5c2f504d49205b98fb89e75
siserin <siserin.desarrollo@gmail.com>
2024-09-27 17:59:20 -0600

    Step 88

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1832,7 +1832,51 @@ Step 87
 The equality operator == is used to check if two values are equal. To compare two values, you'd use a statement like value == 8.
 
 Below done++ inside your loop, add an if statement. The statement should check if done is equal to count using the equality operator.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+  done++;
+
+  if (done == count){
+
+  }
+
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 88
+The equality operator can lead to some strange behavior in JavaScript. For example, "0" == 0 is true, even though one is a string and one is a number.
+
+The strict equality operator === is used to check if two values are equal and share the same type. As a general rule, this is the equality operator you should always use. With the strict equality operator, "0" === 0 becomes false, because while they might have the same value of zero, they are not of the same type.
+
+Update your done == count condition to use the strict equality operator.
 */
+====================================================================================================
 
 const character = "#";
 const count = 8;
@@ -1852,11 +1896,9 @@ let done = 0;
 
 while (continueLoop) {
   done++;
+  if (done === count) {
 
-  if (done == count){
-    
   }
-
 }
 
 let result = ""
@@ -1866,4 +1908,3 @@ for (const row of rows) {
 }
 
 console.log(result);
-====================================================================================================
