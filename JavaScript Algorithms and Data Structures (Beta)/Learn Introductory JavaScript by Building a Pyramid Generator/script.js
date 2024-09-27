e23b145b41fb7d7af9ea1085a5bbccacc37554f8
siserin <siserin.desarrollo@gmail.com>
2024-09-27 17:51:36 -0600

    Step 86

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1759,7 +1759,7 @@ while (condition) {
   logic;
 }
 Use that syntax to declare a while loop with continueLoop as the condition. The body should be empty.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -1772,7 +1772,7 @@ function padRow(rowNumber, rowCount) {
 // TODO: use a different type of loop
 /*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
-}*/
+}
 
 let continueLoop = false;
 let done = 0;
@@ -1792,3 +1792,37 @@ console.log(result);
 ====================================================================================================
 
 
+Step 86
+Right now, if you change continueLoop to true, your while loop will run forever. This is called an infinite loop, and you should be careful to avoid these. An infinite loop can lock up your system, requiring a full restart to escape.
+
+To avoid this, start by using the increment operator to increase the value of the done variable inside your loop.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+  done++;
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
