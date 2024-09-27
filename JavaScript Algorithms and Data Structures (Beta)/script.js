0a8ef5f63848ffdb401c2c33983d09e986c90531
siserin <siserin.desarrollo@gmail.com>
2024-09-26 22:07:10 -0600

    Step 48

--- JavaScript Algorithms and Data Structures (Beta)/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/script.js
@@ -11,7 +11,6 @@ console.log(developer);
 
 
 Step 26
-
 let cities = ["London", "New York", "Munbai"];
 console.log(cities);
 cities[2] = "Mexico City";
@@ -22,7 +21,6 @@ console.log(rows);
 
 
 Step 27
-
 Another method essential for this project is the .pop() method. It removes the last element from an array and returns that element.
 
 When a method returns a value, you can think of it as giving the value back to you, making it available for use in other parts of your code.
@@ -38,7 +36,6 @@ console.log(rows);
 
 
 Step 28
-
 You should have seen "freeCodeCamp" printed to the console. This is because .pop() returns the value that was removed from the array - and you pushed "freeCodeCamp" to the end of the array earlier.
 
 But what does .push() return? Assign your existing rows.push() to a new pushed variable, and log it.
@@ -51,7 +48,6 @@ console.log(pushed);
 
 
 Step 29
-
 Were you expecting to see 4 in the console? .push() returns the new length of the array, after adding the value you give it.
 
 It is important to be aware of what values a method returns. Take some time to experiment with .push() and .pop(). When you are ready, remove all of your .push() and .pop() calls, and your console.log statements.
@@ -67,7 +63,6 @@ console.log(rows);
 
 
 Step 30
-
 Change your rows declaration to be assigned an empty array again.
 
 Also, change your 'Hello' string to use double quotes again. Generally, it does not matter which of the two you prefer, but you will want to be consistent in that choice throughout your project.
@@ -82,7 +77,6 @@ rows = [];
 
 
 Step 31
-
 Declaring a variable with the let keyword allows it to be reassigned. This means you could change character later to be a completely different value.
 
 For this project, you will not want to change these variable values. So instead, you should use const to declare them. const variables are special.
@@ -101,7 +95,6 @@ const rows = [];
 
 
 Step 32
-
 You are now ready to start building your pyramid generator. Your character variable will serve as the building block for the pyramid.
 
 "Hello" might not work very well for that. Change the value of character to be the hash character "#".
@@ -112,7 +105,6 @@ const count = 8;
 const rows = [];
 
 Step 33
-
 To generate a pyramid, you will need to create multiple rows. When you have to perform a task repeatedly until a condition is met, you will use a loop. There are many ways to write a loop.
 
 You are going to start with a basic for loop. for loops use the following syntax:
@@ -129,7 +121,6 @@ for ("iterator"; "condition"; "iteration") {
 
 
 Step 34
-
 Your loop now needs a proper iterator. The iterator is a variable you can declare specifically in your for loop to control how the loop iterates or goes through your logic.
 
 It is a common convention to use i as your iterator variable in a loop. A for loop allows you to declare this in the parentheses (). For example, here is a for loop that declares an index variable and assigns it the value 100.
@@ -149,7 +140,6 @@ for (let i = 0; "condition"; "iteration") {
 }
 
 Step 35
-
 The condition of a for loop tells the loop how many times it should iterate. When the condition becomes false, the loop will stop.
 
 In JavaScript, a Boolean value can be either true or false. These are not strings - you will learn more about the difference later on.
@@ -168,7 +158,6 @@ for (let i = 0; i < count; "iteration") {
 }
 
 Step 36
-
 Your iteration statement will tell your loop what to do with the iterator after each run.
 
 When you reassign a variable, you can use the variable to reference the previous value before the reassignment. This allows you to do things like add three to an existing number. For example, bees = bees + 3; would increase the value of bees by three.
@@ -185,7 +174,6 @@ for (let i = 0; i < count; i = i + 1) {
 }
 
 Step 37
-
 Your loop should now run eight times. Inside the body of the loop, print the value of the i iterator and see what happens.
 
 
@@ -198,7 +186,6 @@ for (let i = 0; i < count; i = i + 1) {
 }
 
 Step 38
-
 You should see the numbers zero through seven printed in your console, one per line. This will serve as the foundation for generating your pyramid.
 
 Replace your log statement with a statement to push i to your rows array.
@@ -213,7 +200,6 @@ for (let i = 0; i < count; i = i + 1) {
 }
 
 Step 39
-
 Unfortunately, now you cannot see what your loop is doing.
 
 Use let to declare a result variable, and assign it an empty string. An empty string is represented by quotation marks with nothing between them, such as "".
@@ -231,7 +217,6 @@ let result = "";
 
 
 Step 40
-
 Add a log statement to print the value of result. Depending on which console you use, you may not see anything printed.
 
 
@@ -249,7 +234,6 @@ console.log(result);
 
 
 Step 41
-
 To manipulate the result string, you will use a different type of loop. Specifically, a for...of loop, which iterates over each item in an iterable object and temporarily assigns it to a variable.
 
 The syntax for a for...of loop looks like:
@@ -282,7 +266,6 @@ console.log(result);
 
 
 Step 42
-
 Remember in your previous loop that you used the addition operator + to increase the value of i by 1.
 
 You can do a similar thing with a string value, by appending a new string to an existing string. For example, hello = hello + " World"; would add the string " World" to the existing string stored in the hello variable. This is called concatenation.
@@ -308,7 +291,6 @@ console.log(result);
 
 
 Step 43
-
 Now all of your numbers are appearing on the same line. This will not work for creating a pyramid.
 
 You will need to add a new line to each row. However, pressing the return key to insert a line break between quotes in JavaScript will result in a parsing error. Instead, you need to use the special escape sequence \n, which is interpreted as a new line when the string is logged. For example:
@@ -336,7 +318,6 @@ console.log(result);
 
 
 Step 44
-
 Printing numbers won't result in a visually appealing pyramid. Now that you're outputting the formatted content of your rows array, it's time to update your original loop.
 
 Instead of pushing i to the array, push the value of your character variable.
@@ -360,7 +341,6 @@ console.log(result);
 
 
 Step 45
-
 Now you have a series of # characters, but the pyramid shape is still missing. Fortunately, the i variable represents the current "row" number in your loop, enabling you to use it for crafting a pyramid-like structure.
 
 To achieve this, you will use the .repeat() method available to strings. This method accepts a number as an argument, specifying the number of times to repeat the target string. For example, using .repeat() to generate the string "Code! Code! Code!":
@@ -390,7 +370,6 @@ console.log(result);
 
 
 Step 46
-
 You're getting closer! At this point, you're encountering what's known as an off-by-one error, a frequent problem in zero-based indexing languages like JavaScript.
 
 The first index of your rows array is 0, which is why you start your for loop with i = 0. But repeating a string zero times results in nothing to print.
@@ -416,7 +395,6 @@ console.log(result);
 
 
 Step 47
-
 The logic for formatting this pyramid is likely going to get complicated, which means it's a great time to extract that code into a function.
 
 A function is a block of code that can be reused throughout your application. Functions are declared with the following syntax:
@@ -428,7 +406,7 @@ function name(parameter) {
 The function keyword tells JavaScript that the name variable is going to be a function. parameter is a variable that represents a value that is passed into the function when it is used. A function may have as many, or as few, parameters as you'd like. Like a for loop, the space between the curly braces is the function body.
 
 Declare a padRow function. Do not create any parameter variables yet. The function body should be empty. Remember that you need to use camel case for your naming convention.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -436,6 +414,42 @@ const rows = [];
 
 function padRow() {} 
 
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+Step 48
+In order to use a function, you need to call it. A function call tells your application to run the code from the function wherever you choose to call it. The syntax for a function call is the function name followed by parentheses. For example, this code defines and calls a test function.
+
+Example Code
+function test() {
+
+}
+test();
+Call your padRow function.
+
+
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+
+}
+
+padRow();
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1))
