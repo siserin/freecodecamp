00ccf3d6e08ef0f1b0f47dea35dc7dfd45acc11a
siserin <siserin.desarrollo@gmail.com>
2024-09-26 22:33:28 -0600

    Step 51

--- JavaScript Algorithms and Data Structures (Beta)/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/script.js
@@ -494,7 +494,7 @@ console.log(result);
 
 Step 50
 Now add a log statement to print the value of your call variable.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -506,6 +506,42 @@ function padRow() {
 const call = padRow();
 
 console.log(call);
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+Step 51
+Your call variable has an undefined value, even though you defined it! This is because your padRow function does not currently return a value. By default, functions return undefined as their value.
+
+In order to return something else, you need to use the return keyword. Here is an example of a function that returns the string "Functions are cool!":
+
+Example Code
+function demo() {
+  return "Functions are cool!";
+}
+Use the return keyword to have your function return the string "Hello!".
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow() {
+    return "Hello!";
+}
+const call = padRow();
+console.log(call);
+
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1))
