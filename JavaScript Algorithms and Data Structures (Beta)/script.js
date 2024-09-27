2f375e1914cb7c3a65ef58904eeca12c8220888c
siserin <siserin.desarrollo@gmail.com>
2024-09-26 22:39:59 -0600

    Step 53

--- JavaScript Algorithms and Data Structures (Beta)/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/script.js
@@ -568,7 +568,7 @@ function demo(name) {
 
 }
 name sounds like a useful parameter, so go ahead and add it to your padRow function.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -594,3 +594,31 @@ for (const row of rows) {
 console.log(result);
 
 
+Step 53
+A function does not have to return a hard-coded value. It can return the value stored in a variable. Parameters are special variables for a function, so they can also be returned.
+
+Change your padRow function to return the name parameter directly.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(name) {
+  return name;
+}
+const call = padRow();
+console.log(call);
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
