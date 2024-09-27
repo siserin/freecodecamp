4e8371254c7c4b06db843ee37066a732cc41f3cd
siserin <siserin.desarrollo@gmail.com>
2024-09-27 15:28:37 -0600

    Step 65

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1007,7 +1007,7 @@ Example Code
 function name(first, second) {
 
 }
-*/
+
 
 
 const character = "#";
@@ -1016,6 +1016,41 @@ const rows = [];
 
 function padRow(rowNumber, rowCount) {
 
+}
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
+Step 65
+Remember in an earlier step, you learned about return values. A function can return a value for your application to consume separately.
+
+In a function, the return keyword is used to specify a return value. For example, this function would return the value given to the first parameter:
+
+Example Code
+function name(parameter) {
+  return parameter;
+}
+Use the return keyword to return the value of the character variable, repeated rowNumber times.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+    return character.repeat(rowNumber);
 }
 
 
