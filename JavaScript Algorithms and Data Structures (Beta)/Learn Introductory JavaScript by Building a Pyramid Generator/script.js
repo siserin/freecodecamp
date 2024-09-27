ea69855d3a8b40f608cb3c81f35fc10984a5f9c9
siserin <siserin.desarrollo@gmail.com>
2024-09-27 15:41:50 -0600

    Step 66

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1043,7 +1043,7 @@ function name(parameter) {
   return parameter;
 }
 Use the return keyword to return the value of the character variable, repeated rowNumber times.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -1056,6 +1056,39 @@ function padRow(rowNumber, rowCount) {
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
+Step 66
+A function call allows you to actually use a function. You may not have been aware of it, but the methods like .push() that you have been using have been function calls.
+
+A function is called by referencing the function's name, and adding (). Here's how to call a test function:
+
+Example Code
+test();
+Replace the character.repeat(i + 1) in your .push() call with a function call for your padRow function.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return character.repeat(rowNumber);
+}
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow())
 }
 
 let result = ""
