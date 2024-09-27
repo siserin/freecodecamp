3693381470f01573ec150ffff21ed6aa2b54a869
siserin <siserin.desarrollo@gmail.com>
2024-09-27 17:04:23 -0600

    Step 78

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1432,7 +1432,7 @@ console.log(result);
 
 // Use /* and */ to turn your current for loop, including the body, into a multi-line comment.
 
-//*/
+/*
 
 const character = "#";
 const count = 8;
@@ -1446,6 +1446,45 @@ for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
 }
 */
+/*
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
+Step 78
+Your pyramid has disappeared again. That's okay - that is to be expected.
+
+Before you create your new loop, you need to learn about if statements. An if statement allows you to run a block of code only when a condition is met. They use the following syntax:
+
+Example Code
+if (condition) {
+  logic
+}
+Create an if statement with the boolean true as the condition. In the body, print the string "Condition is true".
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if (true){
+  console.log("Condition is true");
+}
 
 let result = ""
 
