dfee9d8b7efd38c6b999e7d022e985c890d5cc1e
siserin <siserin.desarrollo@gmail.com>
2024-09-26 22:02:01 -0600

    Step 47

--- JavaScript Algorithms and Data Structures (Beta)/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/script.js
@@ -360,6 +360,7 @@ console.log(result);
 
 
 Step 45
+
 Now you have a series of # characters, but the pyramid shape is still missing. Fortunately, the i variable represents the current "row" number in your loop, enabling you to use it for crafting a pyramid-like structure.
 
 To achieve this, you will use the .repeat() method available to strings. This method accepts a number as an argument, specifying the number of times to repeat the target string. For example, using .repeat() to generate the string "Code! Code! Code!":
@@ -395,11 +396,46 @@ You're getting closer! At this point, you're encountering what's known as an off
 The first index of your rows array is 0, which is why you start your for loop with i = 0. But repeating a string zero times results in nothing to print.
 
 To fix this, add 1 to the value of i in your .repeat() call. Do not assign it back to i like you did in your loop conditions.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+Step 47
+
+The logic for formatting this pyramid is likely going to get complicated, which means it's a great time to extract that code into a function.
+
+A function is a block of code that can be reused throughout your application. Functions are declared with the following syntax:
+
+Example Code
+function name(parameter) {
+
+}
+The function keyword tells JavaScript that the name variable is going to be a function. parameter is a variable that represents a value that is passed into the function when it is used. A function may have as many, or as few, parameters as you'd like. Like a for loop, the space between the curly braces is the function body.
+
+Declare a padRow function. Do not create any parameter variables yet. The function body should be empty. Remember that you need to use camel case for your naming convention.
 */
 
 const character = "#";
 const count = 8;
 const rows = [];
+
+function padRow() {} 
+
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character.repeat(i + 1))
