421f95c57f4f02d83bd659224a275274343644dd
siserin <siserin.desarrollo@gmail.com>
2024-09-27 16:59:22 -0600

    Step 77

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1400,7 +1400,7 @@ Comments can be helpful for explaining why your code takes a certain approach, o
 In JavaScript, you can use // to leave a single-line comment in your code.
 
 Add a single-line comment above your function to remind yourself to change the code to a different kind of loop.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -1414,6 +1414,38 @@ function padRow(rowNumber, rowCount) {
 for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
 }
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+/*
+// Step 77
+//JavaScript also has support for multi-line comments. A multi-line comment starts with /* and ends with */
+
+// Unlike a single-line comment, a multi-line comment will encapsulate multiple lines.
+
+// Use /* and */ to turn your current for loop, including the body, into a multi-line comment.
+
+//*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+/*
+for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+*/
 
 let result = ""
 
