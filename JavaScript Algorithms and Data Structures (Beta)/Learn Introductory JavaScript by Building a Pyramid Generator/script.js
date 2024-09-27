a64f34097a9caaee88f78f80ecd5f5e0e73726be
siserin <siserin.desarrollo@gmail.com>
2024-09-27 16:17:18 -0600

    Step 69

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1143,7 +1143,7 @@ Remember that you can use the + operator to concatenate strings like this:
 
 Example Code
 " " + "string"
-*/
+
 
 
 const character = "#";
@@ -1152,6 +1152,41 @@ const rows = [];
 
 function padRow(rowNumber, rowCount) {
   return " " + character.repeat(rowNumber) + " ";
+}
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+Step 69
+Now it is time for a bit of math. Consider a three-row pyramid. If we want it centered, it would look something like:
+
+Example Code
+··#··
+·###·
+#####
+Empty spaces have been replaced with interpuncts, or middle dots, for readability. If you extrapolate the pattern, you can see that the spaces at the beginning and end of a row follow a pattern.
+
+Update your blank space strings to be repeated rowCount - rowNumber times.
+
+Open up the console to see the result.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
 }
 
 for (let i = 0; i < count; i = i + 1) {
