b03ff90494d8d41d4bc0d533c6e726bf13dd2c8b
siserin <siserin.desarrollo@gmail.com>
2024-09-27 16:01:30 -0600

    Step 68

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1109,7 +1109,7 @@ The values you provide to a function call are referred to as arguments, and you 
 Example Code
 test("Hello");
 Pass i + 1 and count as the arguments to your padRow call. Like parameters, arguments are separated by a comma.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -1119,6 +1119,40 @@ function padRow(rowNumber, rowCount) {
   return character.repeat(rowNumber);
 }
 
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
+Step 68
+You should now see the same bunch of characters in your console. Your padRow function is doing the exact same thing you were doing earlier, but now it's in a reusable section of its own.
+
+Use the addition operator to concatenate a single space " " to the beginning and end of your repeated character string.
+
+Remember that you can use the + operator to concatenate strings like this:
+
+Example Code
+" " + "string"
+*/
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " " + character.repeat(rowNumber) + " ";
+}
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(padRow(i + 1, count));
