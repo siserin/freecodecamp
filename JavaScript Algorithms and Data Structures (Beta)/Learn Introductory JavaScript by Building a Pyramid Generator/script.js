821d892806212aaf6cb405661de3446f7acaffc1
siserin <siserin.desarrollo@gmail.com>
2024-09-27 16:47:16 -0600

    Step 74

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1310,7 +1310,7 @@ Step 73
 Rather than having to pass i + 1 to your padRow call, you could instead start your loop at 1. This would allow you to create a one-indexed loop.
 
 Update your iterator to start at 1 instead of 0.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -1322,6 +1322,34 @@ function padRow(rowNumber, rowCount) {
 
 for (let i = 1; i < count; i++) {
   rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
+Step 74
+The pyramid looks a little funny now. Because you are starting the loop at 1 instead of 0, you do not need to add one to i when you pass it to padRow.
+
+Update the first argument of your padRow call to be i.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i < count; i++) {
+  rows.push(padRow(i, count));
 }
 
 let result = ""
