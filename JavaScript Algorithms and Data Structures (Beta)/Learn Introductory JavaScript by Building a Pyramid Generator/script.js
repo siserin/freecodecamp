3dbbc6361afece4b2240ed49a1f396c853b7bca5
siserin <siserin.desarrollo@gmail.com>
2024-09-27 17:47:53 -0600

    Step 85

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1735,7 +1735,7 @@ function padRow(rowNumber, rowCount) {
 // TODO: use a different type of loop
 /*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
-}*/
+}
 
 let continueLoop = false;
 
@@ -1748,6 +1748,47 @@ for (const row of rows) {
 }
 
 console.log(result);
+====================================================================================================
 
+
+Step 85
+A while loop will run over and over again until the condition specified is no longer true. It has the following syntax:
+
+Example Code
+while (condition) {
+  logic;
+}
+Use that syntax to declare a while loop with continueLoop as the condition. The body should be empty.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+
+}
+
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
 
 
