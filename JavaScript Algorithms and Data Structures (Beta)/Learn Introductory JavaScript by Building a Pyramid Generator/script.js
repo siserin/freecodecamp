41b252db2c4bf7383aaf83dbd1aab06642ff5f41
siserin <siserin.desarrollo@gmail.com>
2024-09-26 23:16:24 -0600

    Step 58

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -756,7 +756,7 @@ function demo(name) {
 }
 demo("Naomi")
 This example would return "Professor Naomi". Update your padRow function to return the value of concatenating your character variable to the beginning of the name parameter.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -765,6 +765,43 @@ const rows = [];
 function padRow(name) {
   return character + name;
 }
+
+const call = padRow("CamperChan");
+console.log(call);
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+Step 58
+Variables can also be declared inside a function. These variables are considered to be in the local scope, or block scope. A variable declared inside a function can only be used inside that function. If you try to access it outside of the function, you get a reference error.
+
+To see this in action, use const to declare a test variable in your padRow function. Initialise it with the value "Testing".
+
+Then, below your function, try to log test to the console. You will see an error because it is not defined outside of the function's local scope. Remove that console.log to pass the tests and continue.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(name) {
+  const test = "Testing";
+  return character + name;
+}
+
+console.log(Testing);
+
 const call = padRow("CamperChan");
 console.log(call);
 
