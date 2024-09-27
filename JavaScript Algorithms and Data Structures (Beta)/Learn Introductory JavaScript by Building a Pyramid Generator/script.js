b186097b3529482ca0f50f312aa614353ff4beb2
siserin <siserin.desarrollo@gmail.com>
2024-09-27 17:06:15 -0600

    Step 79

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1480,9 +1480,42 @@ function padRow(rowNumber, rowCount) {
 // TODO: use a different type of loop
 /*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
-}*/
+}
 
 if (true){
+  console.log("Condition is true");
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
+Step 79
+You'll see the string printed in the console, because true is in fact true.
+
+Change the condition of your if statement to the boolean false.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if (false) {
   console.log("Condition is true");
 }
 
