877f0824f74f7ed3073a1d37e7fef1f6e04e5565
siserin <siserin.desarrollo@gmail.com>
2024-09-27 16:51:38 -0600

    Step 75

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1338,7 +1338,7 @@ Step 74
 The pyramid looks a little funny now. Because you are starting the loop at 1 instead of 0, you do not need to add one to i when you pass it to padRow.
 
 Update the first argument of your padRow call to be i.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -1349,6 +1349,38 @@ function padRow(rowNumber, rowCount) {
 }
 
 for (let i = 1; i < count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
+Step 75
+Unfortunately, now the bottom of the pyramid has disappeared. This is because you have created another off-by-one error.
+
+Your original loop went for i values from 0 to 7, because count is 8 and your condition requires i to be less than count. Your loop is now running for i values from 1 to 7.
+
+Your loop needs to be updated to run when i is 8, too. Looking at your logic, this means your loop should run when i is less than or equal to count. You can use the less than or equal to operator <= for this.
+
+Update your loop condition to run while i is less than or equal to count.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
 }
 
