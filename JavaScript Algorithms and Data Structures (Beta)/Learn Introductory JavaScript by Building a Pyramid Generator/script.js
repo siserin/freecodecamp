c4c434dd00192d63311fbac8242ffcfb995dd0c8
siserin <siserin.desarrollo@gmail.com>
2024-09-27 16:44:45 -0600

    Step 73

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1282,7 +1282,7 @@ Example Code
 let test = 7;
 test++;
 Replace your addition assignment with the increment operator for your loop iteration.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -1293,6 +1293,34 @@ function padRow(rowNumber, rowCount) {
 }
 
 for (let i = 0; i < count; i++) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
+Step 73
+Rather than having to pass i + 1 to your padRow call, you could instead start your loop at 1. This would allow you to create a one-indexed loop.
+
+Update your iterator to start at 1 instead of 0.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i < count; i++) {
   rows.push(padRow(i + 1, count));
 }
 
