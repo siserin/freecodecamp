86e7af04b15ee2088d99797b9db361fad1bd6a9b
siserin <siserin.desarrollo@gmail.com>
2024-09-27 16:35:26 -0600

    Step 70

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1179,7 +1179,7 @@ Empty spaces have been replaced with interpuncts, or middle dots, for readabilit
 Update your blank space strings to be repeated rowCount - rowNumber times.
 
 Open up the console to see the result.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -1187,6 +1187,45 @@ const rows = [];
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(rowNumber) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
+Step 70
+You can pass full expressions as an argument. The function will receive the result of evaluating that expression. For example, these two function calls would yield the same result:
+
+Example Code
+test(2 * 3 + 1);
+test(7);
+Looking at the pattern again:
+
+Example Code
+··#··
+·###·
+#####
+Update the character value to be repeated 2 * rowNumber - 1 times.
+
+Open up the console again to see the updated result.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
 for (let i = 0; i < count; i = i + 1) {
