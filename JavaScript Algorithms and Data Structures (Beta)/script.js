415ed2ffd37f4ca01862ea511cf35dbbf7256d21
siserin <siserin.desarrollo@gmail.com>
2024-09-26 22:37:21 -0600

    Step 52

--- JavaScript Algorithms and Data Structures (Beta)/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/script.js
@@ -530,7 +530,7 @@ function demo() {
   return "Functions are cool!";
 }
 Use the return keyword to have your function return the string "Hello!".
-*/
+
 
 const character = "#";
 const count = 8;
@@ -554,3 +554,43 @@ for (const row of rows) {
 }
 
 console.log(result);
+
+
+Step 52
+When you have a value that is explicitly written in your code, like the "Hello!" string in your function, it is considered to be hard-coded. Hard-coding a value inside a function might not make it as reusable as you'd like.
+
+Instead, you can define parameters for the function. Parameters are special variables that are given a value when you call the function, and can be used in your function to dynamically change the result of the function's code.
+
+To add a parameter to your function, you need to add a variable name inside the parentheses. For example, this demo function has a name parameter:
+
+Example Code
+function demo(name) {
+
+}
+name sounds like a useful parameter, so go ahead and add it to your padRow function.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(name) {
+  return "Hello!";
+}
+const call = padRow();
+console.log(call);
+
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1))
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
