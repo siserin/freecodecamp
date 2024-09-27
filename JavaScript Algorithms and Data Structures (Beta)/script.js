4c32c8cd3c53a39f4736dc3aff6ce2a4f07ed3ee
siserin <siserin.desarrollo@gmail.com>
2024-09-26 21:52:10 -0600

    Step 45

--- JavaScript Algorithms and Data Structures (Beta)/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/script.js
@@ -336,10 +336,11 @@ console.log(result);
 
 
 Step 44
+
 Printing numbers won't result in a visually appealing pyramid. Now that you're outputting the formatted content of your rows array, it's time to update your original loop.
 
 Instead of pushing i to the array, push the value of your character variable.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -347,6 +348,35 @@ const rows = [];
 
 for (let i = 0; i < count; i = i + 1) {
   rows.push(character);
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+Step 45
+Now you have a series of # characters, but the pyramid shape is still missing. Fortunately, the i variable represents the current "row" number in your loop, enabling you to use it for crafting a pyramid-like structure.
+
+To achieve this, you will use the .repeat() method available to strings. This method accepts a number as an argument, specifying the number of times to repeat the target string. For example, using .repeat() to generate the string "Code! Code! Code!":
+
+Example Code
+const activity = "Code! ";
+activity.repeat(3);
+Use the .repeat() method on your character, and give it i for the number.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i));
+  
 }
 
 let result = ""
