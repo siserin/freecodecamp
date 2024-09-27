e180108c41b8a915dc61c38d53305a15c4c9bad2
siserin <siserin.desarrollo@gmail.com>
2024-09-27 17:42:04 -0600

    Step 84

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1698,7 +1698,7 @@ function padRow(rowNumber, rowCount) {
 // TODO: use a different type of loop
 /*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
-}*/
+}
 
 if ("") {
   console.log("Condition is true");
@@ -1714,4 +1714,40 @@ for (const row of rows) {
   result = result + "\n" + row;
 }
 
-console.log(result);*/
+console.log(result);
+====================================================================================================
+
+
+Step 84
+Now that you have practiced working with if...else if...else statements, you can remove them from your code.
+
+Once you complete that, use let to declare a continueLoop variable and assign it the boolean false. Then use let to declare a done variable and assign it the value 0.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+let continueLoop = false;
+
+let done  = 0;
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
