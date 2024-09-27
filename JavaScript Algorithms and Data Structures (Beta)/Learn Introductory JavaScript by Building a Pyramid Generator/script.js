085a104a4c7b2d2aee4d370041582d89e8c2df0a
siserin <siserin.desarrollo@gmail.com>
2024-09-27 17:10:04 -0600

    Step 80

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1513,9 +1513,42 @@ function padRow(rowNumber, rowCount) {
 // TODO: use a different type of loop
 /*for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
-}*/
+}
 
 if (false) {
+  console.log("Condition is true");
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+
+Step 80
+Now the string is no longer printing, because false is not true. But what about other values?
+
+Try changing the condition to the string "false".
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+if ("false") {
   console.log("Condition is true");
 }
 
