d19b393b256fc5824da0f82ce390b1747066edba
siserin <siserin.desarrollo@gmail.com>
2024-09-27 18:19:54 -0600

    Step 92

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1995,7 +1995,44 @@ Step 91
 The strict inequality operator !== allows you to check if two values are not equal, or do not have the same type. The syntax is similar to the equality operator: value !== 4.
 
 Update your while loop condition to check if done is not equal to count.
-*/
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let continueLoop = false;
+let done = 0;
+
+while (done !== count) {
+  done++;
+  rows.push(padRow(done, count));
+  if (done === count) {
+    continueLoop = false;
+  } 
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+tep 92
+Since you have moved the comparison into the while condition, you can remove your entire if statement.*/
 
 const character = "#";
 const count = 8;
@@ -2016,9 +2053,7 @@ let done = 0;
 while (done !== count) {
   done++;
   rows.push(padRow(done, count));
-  if (done === count) {
-    continueLoop = false;
-  } 
+   
 }
 
 let result = ""
