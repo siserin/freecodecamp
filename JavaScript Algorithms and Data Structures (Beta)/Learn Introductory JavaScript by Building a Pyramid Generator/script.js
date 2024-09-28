54e018fd0f1c897e91d4e73b4ae4cb58c1e15e6b
siserin <siserin.desarrollo@gmail.com>
2024-09-27 19:53:10 -0600

    Step 107

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2554,7 +2554,7 @@ Step 106
 You can actually build the inverted pyramid without needing to loop "backwards" like you did.
 
 To do this, you'll need to learn a couple of new array methods. Start by using const to declare a numbers variable. Assign it an array with the elements 1, 2, and 3. Then log the numbers array.
-*/
+
 
 
 const character = "#";
@@ -2565,19 +2565,20 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
+//// TODO: use a different type of loop
+///*for (let i = 1; i <= count; i++) {
+//  rows.push(padRow(i, count));
+//}*/
+//
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}*/
+//
+///*for (let i = count; i > 0; i--) {
+//  rows.push(padRow(i, count));
+//}*/
 
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
-
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
-
+/*
 const numbers = [1,2,3];
 console.log(numbers);
 
@@ -2588,4 +2589,7 @@ for (const row of rows) {
 }
 
 console.log(result);
+====================================================================================================
+*/
+
 ====================================================================================================
