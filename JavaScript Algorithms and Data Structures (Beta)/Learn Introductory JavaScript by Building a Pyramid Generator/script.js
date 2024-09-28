013270c3d0f005694bd219632a8ad1f3c9ea0e9f
siserin <siserin.desarrollo@gmail.com>
2024-09-27 18:29:17 -0600

    Step 94

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2070,6 +2070,44 @@ Step 93
 Your loop is no longer relying on the continueLoop variable. This makes the variable an unused declaration. Generally, you want to avoid unused declarations to prevent future confusion.
 
 Remove your continueLoop variable.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let done = 0;
+
+while (done !== count) {
+  done++;
+  rows.push(padRow(done, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 94
+Your pyramid generator is still working. However, it could be possible to end up with an infinite loop again.
+
+Because you are only checking if done is not equal to count, if done were to be larger than count your loop would go on forever.
+
+Update your loop's condition to check if done is less than or equal to count.
 */
 
 const character = "#";
@@ -2087,7 +2125,7 @@ function padRow(rowNumber, rowCount) {
 
 let done = 0;
 
-while (done !== count) {
+while (done <= count) {
   done++;
   rows.push(padRow(done, count));
 }
