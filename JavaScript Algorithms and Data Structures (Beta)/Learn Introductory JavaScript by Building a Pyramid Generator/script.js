77d9533115d1d949c72515aa8d559ef2af2f60dd
siserin <siserin.desarrollo@gmail.com>
2024-09-27 20:40:40 -0600

    Step 114

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2855,7 +2855,7 @@ console.log(result);
 
 Step 113
 Use an if statement to check if inverted is true. Remember that you do not need to use an equality operator here.
-*/
+
 
 const character = "#";
 const count = 8;
@@ -2872,6 +2872,47 @@ for (let i = 1; i <= count; i++) {
     
   }
   rows.unshift(padRow(i, count));
+}
+
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}*/
+//
+///*for (let i = count; i > 0; i--) {
+//  rows.push(padRow(i, count));
+//}*/
+
+
+/*
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 114
+Now move your .unshift() call into your if block.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+let inverted = true;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  }
+  
 }
 
 /*while (rows.length < count) {
