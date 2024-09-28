0c67df7e623e33204e1aa4ef486469653554d0bf
siserin <siserin.desarrollo@gmail.com>
2024-09-27 19:03:38 -0600

    Step 100

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2285,6 +2285,44 @@ Step 99
 What if you made your pyramid upside-down, or inverted? Time to try it out!
 
 Start by creating a new for loop. Declare your iterator i and assign it the value of count, then use the boolean false for your condition and iteration statements.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+//for (let i = 1; i <= count; i++) {
+//  rows.push(padRow(i, count));
+//}*/
+
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}
+
+/*
+for (let i = count; false; false) {
+
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 100
+Because you are going to loop in the opposite direction, your loop needs to run while i is greater than 0. You can use the greater than operator > for this.
+
+Set your loop's condition to run when i is greater than 0.
 */
 
 const character = "#";
@@ -2304,7 +2342,7 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(rows.length + 1, count));
 }*/
 
-for (let i = count; false; false) {
+for (let i = count; i > 0; false) {
 
 }
 
@@ -2315,4 +2353,5 @@ for (const row of rows) {
 }
 
 console.log(result);
+
 ====================================================================================================
