57f2c4fdcae658ffc3067f2764544c5b718e9f03
siserin <siserin.desarrollo@gmail.com>
2024-09-27 19:40:10 -0600

    Step 104

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2441,6 +2441,45 @@ Step 103
 Just like addition, there are different operators you can use for subtraction. The subtraction assignment operator -= subtracts the given value from the current variable value, then assigns the result back to the variable.
 
 Replace your iterator statement with the correct statement using the subtraction assignment operator.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+//// TODO: use a different type of loop
+///*for (let i = 1; i <= count; i++) {
+//  rows.push(padRow(i, count));
+//}*/
+//
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}*/
+
+/*
+for (let i = count; i > 0; i -= i - 1) {
+  rows.push(padRow(i, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+====================================================================================================
+
+
+Step 104
+Because you are only subtracting one from i, you can use the decrement operator --.
+
+Replace your subtraction assignment with the decrement operator.
 */
 
 const character = "#";
@@ -2460,7 +2499,7 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(rows.length + 1, count));
 }*/
 
-for (let i = count; i > 0; i -= 1) {
+for (let i = count; i > 0; i--) {
   rows.push(padRow(i, count));
 }
 
@@ -2471,5 +2510,4 @@ for (const row of rows) {
 }
 
 console.log(result);
-
 ====================================================================================================
