8bbca3d9541946732adac1be5afed1821233d494
siserin <siserin.desarrollo@gmail.com>
2024-09-27 20:03:04 -0600

    Step 109

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2650,7 +2650,7 @@ numbers.shift();
 The numbers array would be [2, 3].
 
 Directly below your numbers array, declare a shifted variable and assign it the result of calling .shift() on the numbers array. On the next line, log the shifted variable to the console.
-*/
+
 
 
 const character = "#";
@@ -2661,24 +2661,25 @@ function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-// TODO: use a different type of loop
-/*for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-}*/
+//// TODO: use a different type of loop
+///*for (let i = 1; i <= count; i++) {
+//  rows.push(padRow(i, count));
+//}*/
+//
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}*/
+//
+///*for (let i = count; i > 0; i--) {
+//  rows.push(padRow(i, count));
+//}*/
 
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
-
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
-
+/*
 const numbers = [1, 2, 3];
 const shifted = numbers.shift();
 console.log(shifted);
 
-
+//
 const unshifted = numbers.unshift(5);
 console.log(unshifted);
 console.log(numbers);
@@ -2690,4 +2691,7 @@ for (const row of rows) {
 }
 
 console.log(result);
+====================================================================================================
+*/
+
 ====================================================================================================
