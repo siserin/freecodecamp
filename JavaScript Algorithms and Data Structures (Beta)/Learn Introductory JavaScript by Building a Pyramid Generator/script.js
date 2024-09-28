a432cec58cafee4642016e59c5b40bc55417a458
siserin <siserin.desarrollo@gmail.com>
2024-09-27 18:38:17 -0600

    Step 97

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2182,8 +2182,42 @@ console.log(result);
 
 Step 96
 Replace the done reference in your padRow call with rows.length + 1.
-*/
 
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let done = 0;
+
+while (rows.length < count) {
+  done++;
+  rows.push(padRow(rows.length + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 97
+Now you no longer need your done variable. Remove the increment operation from your loop, and the variable declaration for done.
+*/
 
 const character = "#";
 const count = 8;
@@ -2198,10 +2232,8 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(i, count));
 }*/
 
-let done = 0;
 
 while (rows.length < count) {
-  done++;
   rows.push(padRow(rows.length + 1, count));
 }
 
