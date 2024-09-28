cf45b5586b9c97c7743c1db18e9c3d8625f60f7a
siserin <siserin.desarrollo@gmail.com>
2024-09-27 20:28:07 -0600

    Step 112

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2777,12 +2777,53 @@ Step 111
 Your pyramid is no longer inverted. This is because you are adding new rows to the end of the array.
 
 Update your loop body to add new rows to the beginning of the array.
-*/
+
 
 
 const character = "#";
 const count = 8;
 const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+for (let i = 1; i <= count; i++) {
+  rows.unshift(padRow(i, count));
+}
+
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}*/
+//
+///*for (let i = count; i > 0; i--) {
+//  rows.push(padRow(i, count));
+//}*/
+
+/*
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 112
+What if you had a way to toggle between an inverted pyramid and a standard pyramid?
+
+Start by declaring an inverted variable, and assigning it the value true. You are not changing this variable in your code, but you will need to use let so our tests can modify it later.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+let inverted = true;
+
 
 function padRow(rowNumber, rowCount) {
   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
