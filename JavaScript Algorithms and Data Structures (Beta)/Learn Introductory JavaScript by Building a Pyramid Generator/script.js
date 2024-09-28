47bc023b088aa1a22082500b8c360fea5843af92
siserin <siserin.desarrollo@gmail.com>
2024-09-27 18:17:32 -0600

    Step 91

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1952,8 +1952,50 @@ console.log(result);
 
 Step 90
 To make your pyramid generate again, push the result of calling padRow with done and count as the arguments to your rows array, similar to what you did in your first loop.
-*/
 
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+  done++;
+  rows.push(padRow(done,count));
+
+  if (done === count) {
+    continueLoop = false;
+  } 
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+
+Step 91
+The strict inequality operator !== allows you to check if two values are not equal, or do not have the same type. The syntax is similar to the equality operator: value !== 4.
+
+Update your while loop condition to check if done is not equal to count.
+*/
 
 const character = "#";
 const count = 8;
@@ -1971,10 +2013,9 @@ function padRow(rowNumber, rowCount) {
 let continueLoop = false;
 let done = 0;
 
-while (continueLoop) {
+while (done !== count) {
   done++;
-  rows.push(padRow(done,count));
-
+  rows.push(padRow(done, count));
   if (done === count) {
     continueLoop = false;
   } 
