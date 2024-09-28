c7cf5f555408ea621b65a3b5f089732cbf98d6d2
siserin <siserin.desarrollo@gmail.com>
2024-09-27 20:50:20 -0600

    Step 117

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2990,7 +2990,7 @@ console.log(result);
 
 Step 116
 When inverted is false, you want to build a standard pyramid. Use .push() like you have in previous steps to achieve this.
-*/
+
 
 
 const character = "#";
@@ -3011,13 +3011,50 @@ for (let i = 1; i <= count; i++) {
   }
 }
 
-/*while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
-}*/
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}*/
+//
+///*for (let i = count; i > 0; i--) {
+//  rows.push(padRow(i, count));
+//}*/
 
-/*for (let i = count; i > 0; i--) {
-  rows.push(padRow(i, count));
-}*/
+/*
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 117
+Your pyramid generator is now in a finished state, with more functionality than you originally planned! The next step is to clean up your code.
+
+Remove all comments, both single- and multi-line, from your code.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+let inverted = true;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+
 
 let result = ""
 
