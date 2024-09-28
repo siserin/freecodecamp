97a3714ddf4bd5735d67eb71ba5e4bb4d6da723d
siserin <siserin.desarrollo@gmail.com>
2024-09-27 20:14:39 -0600

    Step 110

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2737,7 +2737,6 @@ Step 110
 Sometimes you may wish to bring back previous code that you commented out. You can do so by removing the /* and */ /*around that code. This is called uncommenting.
 
 Uncomment only your first for loop. Leave the single line comment and the other two multi line comments in place.
-
 */
 
 
@@ -2752,6 +2751,45 @@ function padRow(rowNumber, rowCount) {
 // TODO: use a different type of loop
 for (let i = 1; i <= count; i++) {
   rows.push(padRow(i, count));
+}
+
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}*/
+//
+///*for (let i = count; i > 0; i--) {
+//  rows.push(padRow(i, count));
+//}*/
+
+
+/*
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 111
+Your pyramid is no longer inverted. This is because you are adding new rows to the end of the array.
+
+Update your loop body to add new rows to the beginning of the array.
+*/
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+for (let i = 1; i <= count; i++) {
+  rows.unshift(padRow(i, count));
 }
 
 /*while (rows.length < count) {
