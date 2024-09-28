a379a2404015152bcea7135bd780f10622a3e0fd
siserin <siserin.desarrollo@gmail.com>
2024-09-27 18:02:15 -0600

    Step 89

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -1875,8 +1875,46 @@ The equality operator can lead to some strange behavior in JavaScript. For examp
 The strict equality operator === is used to check if two values are equal and share the same type. As a general rule, this is the equality operator you should always use. With the strict equality operator, "0" === 0 becomes false, because while they might have the same value of zero, they are not of the same type.
 
 Update your done == count condition to use the strict equality operator.
-*/
 ====================================================================================================
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+  done++;
+  if (done === count) {
+
+  }
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 89
+When done has reached the value of count, we want the loop to stop executing.
+
+Inside your if body, assign the boolean false to your continueLoop variable.
+*/
 
 const character = "#";
 const count = 8;
@@ -1897,7 +1935,7 @@ let done = 0;
 while (continueLoop) {
   done++;
   if (done === count) {
-
+    continueLoop = false;
   }
 }
 
