ff94df3d9d3e2c218edf6490e2358ae1e28e10af
siserin <siserin.desarrollo@gmail.com>
2024-09-27 19:07:30 -0600

    Step 101

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2323,6 +2323,46 @@ Step 100
 Because you are going to loop in the opposite direction, your loop needs to run while i is greater than 0. You can use the greater than operator > for this.
 
 Set your loop's condition to run when i is greater than 0.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+//// TODO: use a different type of loop
+///*for (let i = 1; i <= count; i++) {
+//  rows.push(padRow(i, count));
+//}*/
+
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}*/
+
+/*
+for (let i = count; i > 0; false) {
+
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+====================================================================================================
+
+
+
+Step 101
+Your iteration statement is also going to be different. Instead of adding 1 to i with each loop, you need to subtract 1.
+
+Like you did earlier with i = i + 1, update your iteration statement to give i the value of subtracting 1 from itself.
 */
 
 const character = "#";
@@ -2342,7 +2382,7 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(rows.length + 1, count));
 }*/
 
-for (let i = count; i > 0; false) {
+for (let i = count; i > 0; i = i - 1) {
 
 }
 
