55aa40598a1d8d0045eb5c6999a9ee1ccaf97a90
siserin <siserin.desarrollo@gmail.com>
2024-09-27 18:36:02 -0600

    Step 96

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2148,7 +2148,42 @@ Arrays have a special length property that allows you to see how many values, or
 Note that rows.length in the padRow call would give you an off-by-one error, because done is incremented before the call.
 
 Update your condition to check if rows.length is less than count.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let done = 0;
+
+while (rows.length < count) {
+  done++;
+  rows.push(padRow(done, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 96
+Replace the done reference in your padRow call with rows.length + 1.
 */
+
 
 const character = "#";
 const count = 8;
@@ -2167,7 +2202,7 @@ let done = 0;
 
 while (rows.length < count) {
   done++;
-  rows.push(padRow(done, count));
+  rows.push(padRow(rows.length + 1, count));
 }
 
 let result = ""
