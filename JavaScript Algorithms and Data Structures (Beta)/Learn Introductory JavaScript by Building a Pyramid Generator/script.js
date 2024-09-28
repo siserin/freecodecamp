574a4132c2fe3aadf82f627fe1bfad75f9f905e8
siserin <siserin.desarrollo@gmail.com>
2024-09-27 19:42:34 -0600

    Step 105

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2480,6 +2480,42 @@ Step 104
 Because you are only subtracting one from i, you can use the decrement operator --.
 
 Replace your subtraction assignment with the decrement operator.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+//// TODO: use a different type of loop
+///*for (let i = 1; i <= count; i++) {
+//  rows.push(padRow(i, count));
+//}*/
+//
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}*/
+
+/*
+for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 105
+Use a multi-line comment to comment out this loop as well, to prepare for the next approach.
 */
 
 const character = "#";
@@ -2498,11 +2534,11 @@ function padRow(rowNumber, rowCount) {
 /*while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
 }*/
-
+/*
 for (let i = count; i > 0; i--) {
   rows.push(padRow(i, count));
 }
-
+*/
 let result = ""
 
 for (const row of rows) {
@@ -2510,4 +2546,3 @@ for (const row of rows) {
 }
 
 console.log(result);
-====================================================================================================
