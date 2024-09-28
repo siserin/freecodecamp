46e5b5ae462738533fce038f2b814e4e61059f06
siserin <siserin.desarrollo@gmail.com>
2024-09-27 18:33:21 -0600

    Step 95

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2108,6 +2108,46 @@ Your pyramid generator is still working. However, it could be possible to end up
 Because you are only checking if done is not equal to count, if done were to be larger than count your loop would go on forever.
 
 Update your loop's condition to check if done is less than or equal to count.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}
+
+let done = 0;
+
+while (done <= count) {
+  done++;
+  rows.push(padRow(done, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+====================================================================================================
+
+
+Step 95
+Using done to track the number of rows that have been generated is functional, but you can actually clean up the logic a bit further.
+
+Arrays have a special length property that allows you to see how many values, or elements, are in the array. You would access this property using syntax like myArray.length.
+
+Note that rows.length in the padRow call would give you an off-by-one error, because done is incremented before the call.
+
+Update your condition to check if rows.length is less than count.
 */
 
 const character = "#";
@@ -2125,7 +2165,7 @@ function padRow(rowNumber, rowCount) {
 
 let done = 0;
 
-while (done <= count) {
+while (rows.length < count) {
   done++;
   rows.push(padRow(done, count));
 }
