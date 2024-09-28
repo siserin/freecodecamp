4d9a90154ca93ee3b2e064ed4e241c7dba9d3ddb
siserin <siserin.desarrollo@gmail.com>
2024-09-27 19:48:06 -0600

    Step 106

--- JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -2516,7 +2516,46 @@ console.log(result);
 
 Step 105
 Use a multi-line comment to comment out this loop as well, to prepare for the next approach.
+
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+//// TODO: use a different type of loop
+///*for (let i = 1; i <= count; i++) {
+//  rows.push(padRow(i, count));
+//}*/
+//
+///*while (rows.length < count) {
+//  rows.push(padRow(rows.length + 1, count));
+//}*/
+///*
+//for (let i = count; i > 0; i--) {
+//  rows.push(padRow(i, count));
+//}
+//*/
+
+/*
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
+
+
+Step 106
+You can actually build the inverted pyramid without needing to loop "backwards" like you did.
+
+To do this, you'll need to learn a couple of new array methods. Start by using const to declare a numbers variable. Assign it an array with the elements 1, 2, and 3. Then log the numbers array.
 */
+
 
 const character = "#";
 const count = 8;
@@ -2534,11 +2573,14 @@ function padRow(rowNumber, rowCount) {
 /*while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
 }*/
-/*
-for (let i = count; i > 0; i--) {
+
+/*for (let i = count; i > 0; i--) {
   rows.push(padRow(i, count));
-}
-*/
+}*/
+
+const numbers = [1,2,3];
+console.log(numbers);
+
 let result = ""
 
 for (const row of rows) {
@@ -2546,3 +2588,4 @@ for (const row of rows) {
 }
 
 console.log(result);
+====================================================================================================
