410ff5612aebcdd89071c1838387dfb4d37a6363
siserin <siserin.desarrollo@gmail.com>
2024-09-29 11:30:21 -0600

    Step 2

--- JavaScript Algorithms and Data Structures (Beta)/Review JavaScript Fundamentals by Building a Gradebook App/script.js
+++ JavaScript Algorithms and Data Structures (Beta)/Review JavaScript Fundamentals by Building a Gradebook App/script.js
@@ -14,7 +14,7 @@ Tips
 
 You can use a loop to iterate over the scores array and add up all the scores.
 You can use the length property to get the total number of scores.
-*/
+
 
 function getAverage(scores) {
     let promedio = 0;
@@ -28,3 +28,55 @@ function getAverage(scores) {
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+
+Step 2
+Now the teacher needs your help converting the student score to a letter grade.
+
+Complete the getGrade function that takes a number score as a parameter. Your function should return a string representing a letter grade based on the score.
+
+Here are the scores and their corresponding letter grades:
+
+Score Range	Grade
+100	"A++"
+90 - 99	"A"
+80 - 89	"B"
+70 - 79	"C"
+60 - 69	"D"
+0 - 59	"F"
+Tips
+
+Remember that you learned about conditional statements (if, else if, and else).
+Remember that you learned about comparison operators (>, <, >=, <=, ===).
+*/
+
+function getAverage(scores) {
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
+    }
+  
+    return sum / scores.length;
+  }
+  function getGrade(score) {
+    let grade = "";
+
+    if (score > 99) {
+        grade = "A++";
+    } else if (score > 89) {
+        grade = "A";
+    } else if (score > 79) {
+        grade = "B";
+    } else if (score > 69) {
+        grade = "C";
+    } else if (score < 60) {
+        grade = "F"
+    }
+
+    return grade;
+  }
+  
+  console.log(getGrade(96));
+  console.log(getGrade(82));
+  console.log(getGrade(56));
